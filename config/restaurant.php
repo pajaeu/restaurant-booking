69482ee0bec098ac3bfae98619e6c9f5980afca0
Paul <fgocsgo@gmail.com>
2024-07-15 15:45:03 +0200

    Little responsivity

--- config/restaurant.php
+++ config/restaurant.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'name' => env('RESTAURANT_NAME', 'Restaurant'),
+    'name' => env('RESTAURANT_NAME', 'SNADNEE Restaurant'),
     'open_hours' => [
         'open' => env('RESTAURANT_OPEN_HOUR', 10),
         'close' => env('RESTAURANT_CLOSE_HOUR', 21),
